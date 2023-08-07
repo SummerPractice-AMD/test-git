ec2a4ba9739be83bf4175ce7762faa17303e7b4d
Serban <eungurea@amd.com>
2023-08-07 12:40:30 +0300

    Add multiply function

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ def hello():
 def sum(a, b):
 	return a + b
 
-
+def multiply(a, b):
+	return a * b
 
 def main():
 	hello()
 	print ("Sum of 5 and 3 = {}".format(sum(5, 3)) )
+	print ("Product of 5 and 3 = {}".format(multiply(5, 3)))
 
 
 if __name__ == '__main__':
