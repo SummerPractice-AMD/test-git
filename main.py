1374ee48d2a63986bf5a4981652b723a793026a4
Serban <eungurea@amd.com>
2023-08-07 12:43:02 +0300

    Add new multiplication

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main():
 	hello()
 	print ("Sum of 5 and 3 = {}".format(sum(5, 3)) )
 	print ("Product of 5 and 3 = {}".format(multiply(5, 3)))
+	print ("Product of 5 and 5 = {}".format(multiply(5, 5)))
 
 
 if __name__ == '__main__':
